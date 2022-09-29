b1df64afc0aa27e76256b40dff70e71cdff22fe9
gabrielseffrin <gabrielfseffrin@gmail.com>
2022-09-28 23:10:04 -0300

    novas pastas e pages

--- src/main/resources/shared.js
+++ src/main/resources/shared.js
@@ -1,4 +1,5 @@
 $(function () {
-    $('#footer').load('footer.html');
-    $('#header').load('header.html');
+    $('#footer').load('/src/main/resources/view/footer.html');
+    $('#header').load('/src/main/resources/view/header.html');
+    $('#min-menu').load('/src/main/resources/view/minMenu.html')
 });
