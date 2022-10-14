3d2ac965e5249f45fbe667c073c8b4ee12cbe48f
gabrielseffrin <gabrielfseffrin@gmail.com>
2022-10-14 19:32:44 -0300

    apresentação dos dados da receita

--- src/main/java/control/RecipeDetail.java
+++ src/main/java/control/RecipeDetail.java
@@ -2,10 +2,10 @@ package control;
 
 import spark.Request;
 import spark.Response;
+import util.FormatList;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 
 import dao.GenericDao;
 import model.Recipe;
@@ -17,33 +17,24 @@ public class RecipeDetail {
         HashMap<String, Object> model = new HashMap<>();
         GenericDao<Recipe> gDao = new GenericDao<>();
         Recipe recipe = new Recipe();
-        String ingredient = "";
+
+        ArrayList<String> dataRecipe = new ArrayList<>();
 
         try {
             long id_recipe = Long.parseLong(req.params("id"));
 
             recipe = gDao.getObjectById(recipe, id_recipe);
-            String[] ingredients = recipe.getIngredients().split(";\\s");
 
-            ArrayList<String> ingredienList = new ArrayList<>();
-
-            for (int i = 0; i < ingredients.length; i++) {
-                ingredienList.add(ingredients[i]);
-
-                System.out.println(ingredient.toString());
-            }
-
-            Iterator<String> aux = ingredienList.iterator();
-            while (aux.hasNext()) {
-                ingredient += aux.next() + "<br>";
-            }
+            dataRecipe.add(FormatList.formatList(recipe.getIngredients()));
+            dataRecipe.add(FormatList.formatList(recipe.getPreparationMode()));
+            dataRecipe.add(FormatList.formatList(recipe.getNote()));
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("erro no banco");
         }
 
-        model.put("ingredients", ingredient);
+        model.put("dataRecipe", dataRecipe);
         model.put("recipe", recipe);
         return new ModelAndView(model, "view/recipeDetail/recipeDetail.vm");
     }
