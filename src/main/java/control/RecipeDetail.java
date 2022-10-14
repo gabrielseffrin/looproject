639f254c87afd9e77c6d1ab8a6c84a486bd30d53
gabrielseffrin <gabrielfseffrin@gmail.com>
2022-10-14 17:46:52 -0300

    iterator ingredients

--- src/main/java/control/RecipeDetail.java
+++ src/main/java/control/RecipeDetail.java
@@ -3,7 +3,9 @@ package control;
 import spark.Request;
 import spark.Response;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import dao.GenericDao;
 import model.Recipe;
@@ -15,16 +17,33 @@ public class RecipeDetail {
         HashMap<String, Object> model = new HashMap<>();
         GenericDao<Recipe> gDao = new GenericDao<>();
         Recipe recipe = new Recipe();
+        String ingredient = "";
 
         try {
             long id_recipe = Long.parseLong(req.params("id"));
 
             recipe = gDao.getObjectById(recipe, id_recipe);
+            String[] ingredients = recipe.getIngredients().split(";\\s");
+
+            ArrayList<String> ingredienList = new ArrayList<>();
+
+            for (int i = 0; i < ingredients.length; i++) {
+                ingredienList.add(ingredients[i]);
+
+                System.out.println(ingredient.toString());
+            }
+
+            Iterator<String> aux = ingredienList.iterator();
+            while (aux.hasNext()) {
+                ingredient += aux.next() + "<br>";
+            }
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("erro no banco");
         }
 
+        model.put("ingredients", ingredient);
         model.put("recipe", recipe);
         return new ModelAndView(model, "view/recipeDetail/recipeDetail.vm");
     }
